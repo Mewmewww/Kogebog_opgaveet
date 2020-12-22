4ef5783911997b1bedb44f367ccc452a016a71d1
Drain <Deniz.yandim00@gmail.com>
2020-12-22 11:43:56 +0100

    Text-files/SetOnButtonAction/Parameters

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,10 +3,8 @@ package sample;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
 
 import javax.swing.text.html.ImageView;
-import java.awt.event.MouseEvent;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Random;
@@ -73,24 +71,20 @@ public class Controller extends Calculator {
 
     ArrayList<String> readLines = new ArrayList<String>();
     // TODO: Add an arbitrary recipe/ingredientslist from google at last.
+
     public void chickenTransfer() {
-        FriedChicken.clear();
-        TestIngredients.clear();
-
-        tabPane.getSelectionModel().select(Opskrift);
-        FriedChicken.appendText("Ingredients for a good fried chicken recipe");
-
-    }
-
-    public void soupTransfer() throws NumberFormatException {
         FriedChicken.clear();
         TestIngredients.clear();
         tabPane.getSelectionModel().select(Opskrift);
         FriedChicken.appendText("Ingredients for a good soup recipe");
 
+        File file = new File("Chicken.txt");
+        File file1 = new File("ChickenTutorial.txt");
+        stealCode(file, file1);
 
-        File file = new File("Soup.txt");
-        File file1 = new File("SoupTutorial.txt");
+    }
+
+    private void stealCode(File file, File file1) {
         try (Scanner input = new Scanner(file)) {
             while (input.hasNextLine()) {
                 TestIngredients.appendText(input.nextLine() + "\n");
@@ -107,6 +101,17 @@ public class Controller extends Calculator {
         } catch (FileNotFoundException ex) {
             ex.printStackTrace();
         }
+    }
+
+    public void soupTransfer() throws NumberFormatException {
+        FriedChicken.clear();
+        TestIngredients.clear();
+        tabPane.getSelectionModel().select(Opskrift);
+        FriedChicken.appendText("Ingredients for a good soup recipe");
+
+        File file = new File("Soup.txt");
+        File file1 = new File("SoupTutorial.txt");
+        stealCode(file, file1);
 
     }
     // TODO: Add an arbitrary recipe/ingredientslist from google at last.
@@ -114,37 +119,24 @@ public class Controller extends Calculator {
     public void steakTransfer() {
         FriedChicken.clear();
         TestIngredients.clear();
-
         tabPane.getSelectionModel().select(Opskrift);
-        FriedChicken.appendText("Ingredients for a good steak recipe");
+        FriedChicken.appendText("Ingredients for a good soup recipe");
 
         File file = new File("Steak.txt");
-        try (Scanner input = new Scanner(file)) {
-            while (input.hasNextLine()) {
-                TestIngredients.appendText(input.nextLine() + "\n");
-                // TODO: Add tutorial on how to cook on the side too. Do with the rest.
-            }
-        } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
-        }
+        File file1 = new File("SteakTutorial.txt");
+        stealCode(file, file1);
     }
     // TODO: Add an arbitrary recipe/ingredientslist from google at last.
 
     public void riceTransfer() {
         FriedChicken.clear();
         TestIngredients.clear();
-
         tabPane.getSelectionModel().select(Opskrift);
-        FriedChicken.appendText("Ingredients for a good rice recipe");
-        File file = new File("Soup.txt");
-        try (Scanner input = new Scanner(file)) {
-            while (input.hasNextLine()) {
-                TestIngredients.appendText(input.nextLine() + "\n");
-                // TODO: Add tutorial on how to cook on the side too. Do with the rest.
-            }
-        } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
-        }
+        FriedChicken.appendText("Ingredients for a good soup recipe");
+
+        File file = new File("Rice.txt");
+        File file1 = new File("RiceTutorial.txt");
+        stealCode(file, file1);
 
     }
     // TODO: Add an arbitrary recipe/ingredientslist from google at last.
@@ -153,17 +145,11 @@ public class Controller extends Calculator {
         FriedChicken.clear();
         TestIngredients.clear();
         tabPane.getSelectionModel().select(Opskrift);
-        FriedChicken.appendText("Ingredients for a good burger recipe");
-        File file = new File("Soup.txt");
-        try (Scanner input = new Scanner(file)) {
-            while (input.hasNextLine()) {
-                TestIngredients.appendText(input.nextLine() + "\n");
-                // TODO: Add tutorial on how to cook on the side too. Do with the rest.
-            }
-        } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
-        }
+        FriedChicken.appendText("Ingredients for a good soup recipe");
 
+        File file = new File("Burger.txt");
+        File file1 = new File("BurgerTutorial.txt");
+        stealCode(file, file1);
     }
     // TODO: Add an arbitrary recipe/ingredientslist from google at last.
 
@@ -171,27 +157,65 @@ public class Controller extends Calculator {
         FriedChicken.clear();
         TestIngredients.clear();
         tabPane.getSelectionModel().select(Opskrift);
-        FriedChicken.appendText("Ingredients for a good lasagna recipe");
-        File file = new File("Soup.txt");
-        try (Scanner input = new Scanner(file)) {
-            while (input.hasNextLine()) {
-                TestIngredients.appendText(input.nextLine() + "\n");
-                // TODO: Add tutorial on how to cook on the side too. Do with the rest.
-            }
-        } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
-        }
+        FriedChicken.appendText("Ingredients for a good soup recipe");
+
+        File file = new File("Lasagna.txt");
+        File file1 = new File("LasagnaTutorial.txt");
+        stealCode(file, file1);
 
     }
 
+    public void setLogsButtonOnAction() {
+        soupSave.setOnAction(e -> {
+            try {
+                logs(new File("Soup.txt"), new File("SoupTutorial.txt"));
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        });
+        steakSave.setOnAction(e -> {
+            try {
+                logs(new File("Steak.txt"), new File("SteakTutorial.txt"));
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        });
+        burgerSave.setOnAction(e -> {
+            try {
+                logs(new File("Burger.txt"), new File("BurgerTutorial.txt"));
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        });
+        lasagnaSave.setOnAction(e -> {
+            try {
+                logs(new File("Lasagna.txt"), new File("LasagnaTutorial.txt"));
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        });
+        riceSave.setOnAction(e -> {
+            try {
+                logs(new File("Rice.txt"), new File("RiceTutorial.txt"));
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        });
+        chickenSave.setOnAction(e -> {
+            try {
+                logs(new File("Chicken.txt"), new File("ChickenTutorial.txt"));
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        });
+    }
+
+
     // Logs - let's the user choose a specific recipe and save it on their own.
-    public void logs() throws IOException {
+    public void logs(File file1, File file2) throws IOException {
 
-
-        File soupIngredients = new File("Soup.txt");
-        File soupTutorial = new File("SoupTutorial.txt");
         // For every time our file has a readable line, it appends that line and does a space so that it becomes readable.
-        try (Scanner soupIngredient = new Scanner(soupIngredients)) {
+        try (Scanner soupIngredient = new Scanner(file1)) {
             while (soupIngredient.hasNextLine()) {
                 saveIngredients.appendText(soupIngredient.nextLine() + "\n");
                 // TODO: Add tutorial on how to cook on the side too. Do with the rest.
@@ -200,7 +224,7 @@ public class Controller extends Calculator {
         catch (FileNotFoundException ex) {
             ex.printStackTrace();
         }
-        try (Scanner soupTutorials = new Scanner(soupTutorial)) {
+        try (Scanner soupTutorials = new Scanner(file2)) {
             while (soupTutorials.hasNextLine()) {
                 saveTutorial.appendText(soupTutorials.nextLine() + "\n");
                 // TODO: Tutorial is done for this, but do the rest of the slavework on the day, just in case im doing it wrong.
@@ -219,7 +243,6 @@ public class Controller extends Calculator {
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
-
                         Platform.runLater(() -> {
                             logsSave.clear();
                             saveDocument.clear();
@@ -227,7 +250,6 @@ public class Controller extends Calculator {
                             saveTutorial.clear();
                         });
                     }).start();
-
                     logsSave.appendText("Error: no document-title found");
                     // Insert platform.run thread to clear both logsSave and the ingredients/tutorial.
                 } catch (NullPointerException e) {
